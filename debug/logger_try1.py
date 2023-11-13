2e94b9e2dd8ec56d108f92a7505de9f7b21114db
pjho4746 <pjho4746@gmail.com>
2023-11-13 13:29:45 +0900

    Logging

--- debug/logger_try1.py
+++ debug/logger_try1.py
@@ -1,3 +1,10 @@
 import logging
 
-logging.debug('위험해!!!!!')
+# 원하는 결과 출력을 위해 지정
+logging.basicConfig(level=logging.DEBUG, format="'%(asctime)s - %(message)s'")
+
+logging.debug('111!!!!!')
+logging.info('222!!!!!')
+logging.warning('333!!!!!')
+logging.error('444!!!!!')
+logging.critical('555!!!!!')
