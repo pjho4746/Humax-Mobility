09dcee8fb5e61d4c8fc8418518a1787ccaf0ed06
pjho4746 <pjho4746@gmail.com>
2023-11-14 13:28:14 +0900

    금액 계산

---
+++ skill/price01.py
@@ -0,0 +1,12 @@
+# 숫자 계산 시 자료형
+
+int_x = 10
+float_y = 2.5
+calc_z = int_x/float_y
+
+print(type(int_x))
+print(type(float_y))
+print(type(calc_z))
+
+print(isinstance(calc_z, int))
+print(isinstance(calc_z, float))
